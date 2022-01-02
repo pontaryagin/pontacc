80e87a2c1ee7d4053649e2ce8ac4f5dd20abbd0a
pontaryagin <ponta1496@gmail.com>
2022-01-02 20:45:00 +0900

    refactor

--- parser.h
+++ parser.h
@@ -3,12 +3,15 @@
 #include "tokenizer.h"
 #include "codegen.h"
 
-static void expect_punct(const vector<Token>& tokens, int pos, const string& op){
+static bool is_punct(const vector<Token>& tokens, int pos, const string& op){
     if (pos >= tokens.size()){
         verror_at(tokens.back(), "'" + op + "' is expected", true);
-        return;
     }
-    if (tokens.at(pos).punct != op){
+    return tokens.at(pos).kind == TokenKind::Punct && tokens.at(pos).punct == op;
+}
+
+static void expect_punct(const vector<Token>& tokens, int pos, const string& op){
+    if (!is_punct(tokens, pos, op)){
         verror_at(tokens.at(pos), "'" + op + "' is expected");
         return;
     }
@@ -19,14 +22,9 @@ pair<unique_ptr<INode>,int> parse_expr(const vector<Token>& tokens, int start_po
 //  primary = num | ident | "(" expr ")"
 pair<unique_ptr<INode>,int> parse_primary(const vector<Token>& tokens, int start_pos){
     auto token_val = tokens.at(start_pos).punct;
-    if (token_val == "("){
+    if (is_punct(tokens, start_pos, "(")){
         auto [pNode, pos] = parse_expr(tokens, start_pos+1);
-        if (pos >= tokens.size()){
-            verror_at(tokens.at(start_pos), "'(' was not closed");
-        }
-        if (tokens.at(pos).punct != ")") {
-            verror_at(tokens.at(pos), "')' should come here");
-        }
+        expect_punct(tokens, pos, ")");
         return {move(pNode), pos+1};
     }
     else if (tokens.at(start_pos).kind == TokenKind::Ident){
@@ -119,7 +117,7 @@ pair<unique_ptr<INode>,int> parse_equality(const vector<Token>& tokens, int star
 // assign     = equality ("=" assign)?
 pair<unique_ptr<INode>,int> parse_assign(const vector<Token>& tokens, int start_pos){
     auto [pNode, pos] = parse_equality(tokens, start_pos);
-    if (pos < tokens.size() && tokens.at(pos).punct == "="){
+    if (is_punct(tokens, pos, "=")){
         auto [pNode2, pos2] = parse_assign(tokens, pos+1);
         pNode = make_unique<NodeAssign>(tokens.at(pos), move(pNode), move(pNode2));
         pos = pos2;
@@ -136,7 +134,7 @@ pair<unique_ptr<INode>,int> parse_statement(const vector<Token>& tokens, int pos
 // compound-statement = statement* "}"
 pair<unique_ptr<INode>,int> parse_compound_statement(const vector<Token>& tokens, int pos){
     vector<unique_ptr<INode>> pNodes;
-    while (tokens.at(pos).punct != "}"){
+    while (!is_punct(tokens, pos, "}")){
         auto [pNode, pos_] = parse_statement(tokens, pos);
         pos = pos_;
         pNodes.emplace_back(move(pNode));
@@ -144,22 +142,20 @@ pair<unique_ptr<INode>,int> parse_compound_statement(const vector<Token>& tokens
     return {make_unique<NodeCompoundStatement>(move(pNodes)), pos+1};
 }
 
-// statement = expr ";" | "{" compound-statement | "return" expr ";"
+// statement = "{" compound-statement | expr ";" | "return" expr ";"
 pair<unique_ptr<INode>,int> parse_statement(const vector<Token>& tokens, int start_pos){
     optional<int> ret_pos;
     if (tokens.at(start_pos).kind == TokenKind::Keyword){
         ret_pos = start_pos;
         start_pos += 1;
     }
-    else if (tokens.at(start_pos).punct == "{"){
+    else if (is_punct(tokens, start_pos, "{")){
         return parse_compound_statement(tokens, start_pos+1);
     }
     auto [pNode, pos] = parse_expr(tokens, start_pos);
-    if (pos >= tokens.size() || tokens.at(pos).punct != ";"){
-        verror_at(tokens.at(pos-1), "';' is expected", true);
-    }
+    expect_punct(tokens, pos, ";");
     if (ret_pos){
-        pNode = make_unique<NodeRet>(tokens.at(*ret_pos),move(pNode));
+        pNode = make_unique<NodeRet>(tokens.at(*ret_pos), move(pNode));
     }
     pos += 1;
     return {move(pNode), pos};
