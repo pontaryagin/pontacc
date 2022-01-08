a5c57e69be224765a365cc9874323ae22bf9be6d
pontaryagin <ponta1496@gmail.com>
2022-01-08 15:09:51 +0900

    refactor (use rax as return value)

--- codegen.h
+++ codegen.h
@@ -7,15 +7,23 @@ void gen_header(){
 }
 
 static void ass_pop(string_view reg){
-    cout << "  pop %" << reg << endl;
+    cout << "  pop " << reg << endl;
 }
 
 static void ass_push(string_view reg){
-    cout << "  push %" << reg << endl;
+    cout << "  push " << reg << endl;
 }
 
 static void ass_push(int num){
     cout << "  push $" << num << endl;
+}
+
+static void ass_mov(string from, string to){
+    cout << "  mov " << from << ", " << to << endl;
+}
+
+static void ass_mov(int from, string to){
+    cout << "  mov " << "$" << from << ", " << to << endl;
 }
 
 static void ass_label(string s){
@@ -121,72 +129,60 @@ struct Node
 static void generate(const Node& node);
 
 static void generate(const NodeNull& node){
-    ass_push("rax");
 }
 
 static void generate(const NodeIf& node){
     generate(*node.expr);
     auto count = to_string(node.count);
-    ass_pop("rax");
     cout << "cmp $0" << ", %rax" << endl;
     cout << "je " << ".L.else." << count << endl;
     generate(*node.statement_if);
-    ass_pop("rax");
     cout << "jmp " << ".L.end." << count << endl;
     ass_label(".L.else." + count);
     if (node.statement_else){
         generate(*node.statement_else);
-        ass_pop("rax");
     }
     ass_label(".L.end." + count);
-    ass_push("rax");
 }
 
 static void generate(const NodeFor& node){
     auto count = to_string(node.count);
     generate(*node.expr_init);
-    ass_pop("rax");
     ass_label(".L.for." + count);
     if (get_if<NodeNull>(&node.expr_condition->val)){
-        ass_push(1);
+        ass_mov(1, "%rax");
     }
     else {
         generate(*node.expr_condition);
     }
-    ass_pop("rax");
     cout << "cmp $0" << ", %rax" << endl;
     cout << "je " << ".L.end." + count << endl;
     generate(*node.statement);
-    ass_pop("rax");
     generate(*node.expr_increment);
-    ass_pop("rax");
     cout << "jmp " << ".L.for." + count << endl;
     ass_label(".L.end." + count);
-    ass_push("rax");
 }
 
-
-
 static void generate(const NodeNum& node){
-    ass_push(node.num);
+    ass_mov(node.num, "%rax");
 }
 static void generate(const NodeCompoundStatement& node){
     for (auto& pNode: node.pNodes){
         generate(*pNode);
-        ass_pop("rax");
     }
-    ass_push("rax");
 }
+
 static void generate(const NodeIdent& node){
-    cout << "  mov " << ass_stack_reg(node.stack_num) << ", %rax" << endl;
-    ass_push("rax"); 
+    ass_mov(ass_stack_reg(node.stack_num), "%rax");
 }
 
 static void generate(const NodePunct& node){
     generate(*node.lhs);
+    ass_push("%rax");
     generate(*node.rhs);
-    ass_pop("rdi");
-    ass_pop("rax");
+    ass_mov("%rax", "%rdi");
+    ass_pop("%rax");
+
     auto& type = node.token.punct;
     if(type == "+"){
         cout << "  add %rdi, %rax" << endl;
@@ -234,20 +230,16 @@ static void generate(const NodePunct& node){
     else{
         throw;
     }
-    ass_push("rax");
 }
 
 static void generate(const NodeAssign& node){
     generate(*node.rhs);
-    ass_pop("rax");
     auto& ident = get<NodeIdent>(node.lhs->val);
     cout << "  mov %rax, " << ass_stack_reg(ident.stack_num) << endl;
-    ass_push("rax");
 }
 
 static void generate(const NodeRet& node){
     generate(*node.pNode);
-    ass_pop("rax");
     cout << "  jmp .L.return" << endl;
 }
 
