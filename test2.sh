9ac01657f8ef61a36c429fef68dd854d503c9075
pontaryagin <ponta1496@gmail.com>
2022-07-17 00:32:49 +0900

    dd

--- test2.sh
+++ test2.sh
@@ -30,7 +30,7 @@ assert() {
 }
 
 assert 7 'int main() { return add2(3,4); } int add2(int x, int y) { return x+y; }'
-# assert 1 'int main() { return sub2(4,3); } int sub2(int x, int y) { return x-y; }'
+# assert 1 'int main() { return sub2(4,3,1); } int sub2(int x, int y) { return x-y; }'
 # assert 55 'int main() { return fib(9); } int fib(int x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
 
 echo OK
